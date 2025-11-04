b7fd2bf3a5883e9e4db3fd44347cc2bef3d75f5e
MoriyaDev <moriyaprober@gmail.com>
2025-11-04 13:07:45 +0200

    Group creation process

--- server/src/models/candidate_model.js
+++ server/src/models/candidate_model.js
@@ -1,18 +1,14 @@
 const mongoose = require('mongoose');
 
-// סכימה למועמד
 const candidateSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },    // מזהה ייחודי של המועמד
-  groupId: { type: String, required: true },            // מזהה הקבוצה שאליה שייך המועמד
-  name: { type: String, required: true },               // שם המועמד או המפלגה
-  description: { type: String },                        // תיאור קצר או מצע
-  photoUrl: { type: String },                            // קישור לתמונה / לוגו
-  symbol: { type: String },                              // סימן קצר שמייצג את המועמד
-  createdAt: { type: Date, default: Date.now },         // תאריך הוספת המועמד
-  votesCount: { type: Number, default: 0 }             // מספר ההצבעות שקיבל
+  name: { type: String, required: true },
+  description: { type: String },
+  photoUrl: { type: String },
+  symbol: { type: String },
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
+  createdAt: { type: Date, default: Date.now },
+  votesCount: { type: Number, default: 0 }
 });
 
-// יצירת המודל
 const Candidate = mongoose.model('Candidate', candidateSchema);
-
 module.exports = Candidate;
