8dcb7da0c0f81a4c8677f8df781ce4e5a6ebe338
MoriyaDev <moriyaprober@gmail.com>
2025-11-05 14:35:26 +0200

    Home page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,18 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from './slices/authSlice';
-
-import RegisterPage from './pages/RegisterPage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import ProfilePage from './pages/ProfilePage.jsx';
-import UsersPage from './pages/UsersPage.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
+import HomeRoute from './pages/Home/HomePage.jsx';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const token = useSelector(s => s.auth.token);
-
   return (
-    <div style={{ padding: 16 }}>
-      <nav style={{ display: 'flex', gap: 12 }}>
-        <Link to="/register">הרשמה</Link>
-        <Link to="/login">התחברות</Link>
-        <Link to="/me">פרופיל</Link>
-        <Link to="/users">משתמשים</Link>
-        {token && <button onClick={() => dispatch(logout())}>Logout</button>}
-      </nav>
+    <div>
+      <NavBar />
 
       <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route path="/me" element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/users" element={<UsersPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/" element={<HomeRoute />} />
+        {/* <Route path="/me" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} /> */}
+        {/* <Route path="/users" element={<UsersPage />} /> */}
       </Routes>
     </div>
   );
