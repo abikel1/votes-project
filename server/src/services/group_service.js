2da0b1d73b651d61872de3975792f5bc37c1e002
MoriyaDev <moriyaprober@gmail.com>
2025-11-05 11:21:11 +0200

    Editing, deleting and accepting groups

--- server/src/services/group_service.js
+++ server/src/services/group_service.js
@@ -1,5 +1,6 @@
 const Group = require('../models/group_model');
 
+// יצירת קבוצה
 async function createGroupService(groupData) {
   const group = new Group({
     name: groupData.name,
@@ -9,10 +10,38 @@ async function createGroupService(groupData) {
     maxWinners: groupData.maxWinners,
     shareLink: groupData.shareLink
   });
-
   await group.save();
-  console.log('✅ Group created:', group);
   return group;
 }
 
-module.exports = { createGroupService };
+// עריכת קבוצה
+async function updateGroupService(groupId, updateData) {
+  const group = await Group.findByIdAndUpdate(groupId, updateData, { new: true });
+  return group;
+}
+
+// מחיקת קבוצה
+async function deleteGroupService(groupId) {
+  const group = await Group.findByIdAndDelete(groupId);
+  return group;
+}
+
+// קבלת קבוצה לפי ID
+async function getGroupByIdService(groupId) {
+  const group = await Group.findById(groupId).populate('candidates');
+  return group;
+}
+
+// קבלת כל הקבוצות
+async function getAllGroupsService() {
+  const groups = await Group.find().populate('candidates');
+  return groups;
+}
+
+module.exports = {
+  createGroupService,
+  updateGroupService,
+  deleteGroupService,
+  getGroupByIdService,
+  getAllGroupsService
+};
