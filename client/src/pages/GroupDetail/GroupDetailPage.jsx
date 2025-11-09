6540ca0d9aa7d5f20e1725fa6e1050251aea1776
MoriyaDev <moriyaprober@gmail.com>
2025-11-09 13:19:02 +0200

    GroupDetailPage

--- client/src/pages/GroupDetail/GroupDetailPage.jsx
+++ client/src/pages/GroupDetail/GroupDetailPage.jsx
@@ -1,1 +1,36 @@
-  
+  import { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGroups, selectGroupsWithOwnership } from '../../slices/groupsSlice';
+
+export default function GroupDetailPage() {
+    const { groupId } = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const groups = useSelector(selectGroupsWithOwnership);
+
+    useEffect(() => {
+        dispatch(fetchGroups());
+    }, [dispatch]);
+
+    const group = groups.find(g => g._id === groupId);
+
+    if (!group) return <div>טוען...</div>;
+
+    return (
+        <div className="page-wrap">
+            <h2>{group.name}</h2>
+            <p>{group.description}</p>
+
+            <div>
+                <small>נוצר:</small> {group.creationDate}<br/>
+                <small>סיום:</small> {group.endDate}
+            </div>
+
+            {/* כפתור לחדר ההצבעה */}
+            <button onClick={() => navigate(`/groups/${groupId}/candidates`)}>
+                ללכת להצבעה
+            </button>
+        </div>
+    );
+}
